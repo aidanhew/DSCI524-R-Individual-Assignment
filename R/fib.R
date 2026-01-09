36032baedc43c893b9736354d010c2d9b421a439
aidanhew <aidan.hew@outlook.com>
2026-01-08 19:43:26 -0800

    create documentation

--- R/fib.R
+++ R/fib.R
@@ -1,3 +1,13 @@
+#' Returns fibonacci numbers up to a given integer
+#'
+#' @param n Integer giving the upper bound
+#'
+#' @returns Integer vector containing numbers less than n
+#' @export
+#'
+#' @examples
+#' fib_before_n(10)
+#' [1] 0 1 1 2 3 5 8
 fib_before_n <- function(n) {
   if (!is.numeric(n) || length(n) != 1 || n %% 1 != 0) {
     stop("n must be a single integer", call. = FALSE)
